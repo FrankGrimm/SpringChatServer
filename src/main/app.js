5d3521a6ba895e7a68cfb7ee27b90299fee38de0
Mary Grygleski IBM <mary.grygleski@ibm.com>
2021-03-31 12:18:53 -0500

    WebSocket client commits (from 3/24 Twitch session)

---
+++ src/main/app.js
@@ -0,0 +1,52 @@
+var ws = null;
+var url = "ws://localhost:18080/echo";
+
+function setConnected(connected)
+{
+    document.getElementById('connect').disabled = connected;
+    document.getElementById('disconnect').disabled = !connected;
+    document.getElementById('echo').disabled = !connected;
+}
+
+function connect()
+{
+    ws = new WebSocket(url);
+    ws.onopen = function() {
+        setConnected(true);
+        log('Info: Connection established!');
+    };
+
+    ws.onmessage = function(event) {
+        log(event.data);
+    }
+
+    ws.onclose = function(event) {
+        setConnected(false);
+        log('Info: Connection closed!');
+    };
+}
+
+function disconnect() {
+    if (ws != null) {
+        ws.close();
+        ws = null;
+    }
+    setConnected(false);
+}
+
+function echo() {
+    if (ws != null) {
+        var message = document.getElementById('message').value;
+        log('Sent to server ::' + message);
+        ws.send(message);
+    } else {
+        alert('connection is not established, please connect first before sending!');
+    }
+}
+
+function log(message) {
+    var console = document.getElementById('logging');
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(message));
+    console.appendChild(p);
+}
